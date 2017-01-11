adc07bb924f6cb5c420ed730e2491fcb1672b0a0
Mahmut Ali ÖZKURAN <ozkuran@gmail.com>
2017-01-11 22:58:38 +0300

    WeekOfCode28 The Great XOR

---
+++ WeekOfCode28.TheGreatXOR/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeekOfCode28.TheGreatXOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int q = Convert.ToInt32(Console.ReadLine());
+            List<int> numbersThatFit = new List<int>();
+            for (int a0 = 0; a0 < q; a0++)
+            {
+                long x = Convert.ToInt64(Console.ReadLine());
+                int temporaryCounter = 0;
+                for (int i = 1; i < x; i++)
+                {
+                    if ((x ^ i) > x)
+                    {
+                        temporaryCounter++;
+                    }
+                }
+                numbersThatFit.Add(temporaryCounter);
+            }
+
+            foreach (var i in numbersThatFit)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
